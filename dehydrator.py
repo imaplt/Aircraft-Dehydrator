eb7f54714f753bfde4802213464d684ed3279825
imaplt <chris.auron@gmail.com>
2024-08-07 13:29:23 -0400

    Display rotation changes/fixes etc

--- dehydrator.py
+++ dehydrator.py
@@ -153,9 +153,9 @@ def lcd_display(screen_no):
         lcd_lines[3] = f"Ext Min:{EXTERNAL_LOW_TEMP}C {EXTERNAL_LOW_HUMIDITY}%"
         lcd2004Display.display_four_rows_center(lcd_lines, justification='left')
     else:
-        lcd_lines[0] = f"Fan Count: {CYCLE_COUNT}"
-        lcd_lines[1] = f"Fan Duration: {TOTAL_CYCLE_DURATION}"
-        lcd_lines[2] = ""
+        lcd_lines[0] = "Fan Cycles/Duration:"
+        lcd_lines[1] = f"Count: {CYCLE_COUNT}"
+        lcd_lines[2] = f"Duration: {TOTAL_CYCLE_DURATION}"
         lcd_lines[3] = ""
         lcd2004Display.display_four_rows_center(lcd_lines, justification='left')
 
