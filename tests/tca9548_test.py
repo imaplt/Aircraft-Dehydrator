8a5dd1672508f7da57493816ebe2d9e67df41fe6
imaplt <chris.auron@gmail.com>
2025-08-18 19:01:47 -0400

    New MUX Test File

--- tests/tca9548_test.py
+++ tests/tca9548_test.py
@@ -3,8 +3,11 @@
 
 # This example shows using TCA9548A to perform a simple scan for connected devices
 import board
-
 import adafruit_tca9548a
+import qwiic_tca9548a
+import qwiic_i2c
+import time
+import sys
 
 # Create I2C bus as normal
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -19,3 +22,38 @@ for channel in range(8):
         addresses = tca[channel].scan()
         print([hex(address) for address in addresses if address != 0x70])
         tca[channel].unlock()
+
+print("\nSparkFun TCA9548A 8-Channel Mux Example\n")
+
+# Create an instance of the Qwiic TCA9548A object
+myTca = qwiic_tca9548a.QwiicTCA9548A()
+
+# Check if the device is connected
+if not myTca.connected:
+    print("The Qwiic TCA9548A 8-Channel Mux device isn't connected to the system. Please check your connection",
+          file=sys.stderr)
+
+
+# Get the I2C driver
+i2c = qwiic_i2c.getI2CDriver()
+
+while True:
+    print("\n--- Enabling Channels 0 and 1 ---")
+    myTca.disable_all()  # Disable all channels first
+    myTca.enable_channels([0, 1]) # Enable specific channels
+    myTca.list_channels() # List current channel status
+
+    print("Checking for I2C devices on ports 0 and 1:")
+    devices = i2c.scan()
+    print("Devices found:", devices)
+    time.sleep(2)
+
+    print("\n--- Enabling Channels 2 and 3 ---")
+    myTca.disable_all()
+    myTca.enable_channels([2, 3])
+    myTca.list_channels()
+
+    print("Checking for I2C devices on ports 2 and 3:")
+    devices = i2c.scan()
+    print("Devices found:", devices)
+    time.sleep(2)
