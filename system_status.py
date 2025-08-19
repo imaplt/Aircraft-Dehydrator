f542953e17d231c854a6f8497e08835e15a08d7f
imaplt <chris.auron@gmail.com>
2025-08-18 23:00:44 -0400

    Updating system status file for sensors using mux.

--- system_status.py
+++ system_status.py
@@ -67,14 +67,14 @@ def detect_mux_and_sht4X(devices, overall_status_var=None):
         # Internal sensor
         if INTERNAL_SENSOR_PORT < len(mux):
             sht4X_internal = adafruit_sht4x.SHT4x(mux[INTERNAL_SENSOR_PORT])
-            devices["SHT45_Internal"]["status"] = (
+            devices["SHT4X_Internal"]["status"] = (
                 f"Detected, temperature: {sht4X_internal.temperature:.2f} C, humidity: {sht4X_internal.relative_humidity:.2f} %"
             )
 
         # External sensor
         if EXTERNAL_SENSOR_PORT < len(mux):
             sht4X_external = adafruit_sht4x.SHT4x(mux[EXTERNAL_SENSOR_PORT])
-            devices["SHT45_External"]["status"] = (
+            devices["SHT4X_External"]["status"] = (
                 f"Detected, temperature: {sht4X_external.temperature:.2f} C, humidity: {sht4X_external.relative_humidity:.2f} %"
             )
 
